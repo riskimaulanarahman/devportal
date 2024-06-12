1c17556c991d2d7e74ca5a6e6898b2c6cd34c2f6
riskimaulanarahman <riskimaulanarahman@gmail.com>
2024-06-12 09:12:27 +0800

    add jdi panduan

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -354,6 +354,7 @@
                             <li class="mb-1"><button class="btn btn-primary" onclick="window.open('{{ env('APP_URL') }}/public/upload/panduan/DevPortal Panduan.pdf', '_blank')">Project Management & Ticket Request</button></li>
                             <li class="mb-1"><button class="btn btn-primary" onclick="window.open('{{ env('APP_URL') }}/public/upload/panduan/SKYMAP System.pdf', '_blank')">SKYMAP</button></li>
                             <li class="mb-1"><button class="btn btn-primary" onclick="window.open('{{ env('APP_URL') }}/public/upload/panduan/HRSC Panduan.pdf', '_blank')">HR Service Care</button></li>
+                            <li class="mb-1"><button class="btn btn-primary" onclick="window.open('{{ env('APP_URL') }}/public/upload/panduan/JDI Online.pdf', '_blank')">JDI Online</button></li>
                         </ul>
                     </div>
                 </div><!-- /.modal-content -->
