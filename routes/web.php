2a33f1641418061199e2c3542ca90f3a03b6de3d
riskimaulanarahman <riskimaulanarahman@gmail.com>
2025-03-22 00:54:21 +0800

    fix route

--- routes/web.php
+++ routes/web.php
@@ -16,20 +16,19 @@ Route::get('index/{locale}', [App\Http\Controllers\HomeController::class, 'lang'
 
 // GH & Mess Booking
 Route::get('ghm-autoappr',[App\Http\Controllers\Submission\GhmRequestController::class, 'ghmautoapproved'])->name('ghm-autoappr');
+Route::get('ghm_booking',[App\Http\Controllers\Submission\GhmRequestController::class, 'dashboard'])->name('ghm_booking'); //Ghm_request
 
 Route::middleware(['session.check'])->group(function () {
     // Import
     Route::get('import-mcop',[App\Http\Controllers\Submission\HRIS\MCOP\McopMonitoringController::class, 'import'])->name('import-mcop');
     Route::post('import-mcop-csv', [App\Http\Controllers\Submission\HRIS\MCOP\McopMonitoringController::class, 'importCsv'])->name('import.mcopcsv');
-
+    
     if(Schema::hasTable('reference.side_menus')) {
         $sidemenu = SideMenu::select('route')->where('route','!=','api')->get();
-            foreach ($sidemenu as $menu_item) {
-                Route::get('{menu_item}', [App\Http\Controllers\GeneratemenuController::class, 'index'])->name('index');
-            }
+        foreach ($sidemenu as $menu_item) {
+            Route::get('{menu_item}', [App\Http\Controllers\GeneratemenuController::class, 'index'])->name('index');
+        }
     }
-
-    Route::get('ghm_booking',[App\Http\Controllers\Submission\GhmRequestController::class, 'dashboard'])->name('ghm_booking'); //Ghm_request
 
     // Scheduler
     Route::get('/mom-reminder/{mode}',[App\Http\Controllers\Submission\MomTaskUpdateController::class, 'reminderNotificationMessage'])->name('mom-reminder');
